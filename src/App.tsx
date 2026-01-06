613a998672f8213dc9a92ab97c92151520f0933e
Vitor Leite <vhsleite@gmail.com>
2026-01-06 12:59:24 +0200

    created initial html and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app" data-theme="clean">
+        <header className="competitionHeader">
+          <h1>Premier League</h1>
+        </header>
+        <main className="column lg">
+          <section className="card">
+            <div className="title">Add Team</div>
+            <div className="row">
+              <input type="text" placeholder="Team Name" className="grow" />
+              <button className="primary">Add</button>
+            </div>
+          </section>
+
+          <section className="card">
+            <div className="title">Add Score</div>
+            <div className="row">
+              <div className="column">
+                <input type="text" placeholder="Home Team" className="grow" />
+                <input type="text" placeholder="Home Score" className="grow" />
+              </div>
+              <div className="column">
+                <input type="text" placeholder="Away Team" className="grow" />
+                <input type="text" placeholder="Away Score" />
+              </div>
+            </div>
+            <button className="primary">Add Score</button>
+          </section>
+
+          <section className="standingsGrid">
+            <div className="standingsGridHeaders">
+              <div>Team</div>
+              <div className="center">P</div>
+              <div className="center">W</div>
+              <div className="center">D</div>
+              <div className="center">L</div>
+              <div className="center">Pts</div>
+            </div>
+
+            <div className="standingsGridRow">
+              <div>Man Utd</div>
+              <div className="center">10</div>
+              <div className="center">4</div>
+              <div className="center">2</div>
+              <div className="center">2</div>
+              <div className="center strong">14</div>
+            </div>
+            <div className="standingsGridRow">
+              <div>Man Utd</div>
+              <div className="center">10</div>
+              <div className="center">4</div>
+              <div className="center">2</div>
+              <div className="center">2</div>
+              <div className="center strong">14</div>
+            </div>
+            <div className="standingsGridRow">
+              <div>Man Utd</div>
+              <div className="center">10</div>
+              <div className="center">4</div>
+              <div className="center">2</div>
+              <div className="center">2</div>
+              <div className="center strong">14</div>
+            </div>
+          </section>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
