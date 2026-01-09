25b707cdf31ae7d3fd1acd10904f68cf16d3a0d4
Vitor Leite <vhsleite@gmail.com>
2026-01-09 12:40:36 +0200

    setup github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/kt-frontend-challenge-sports-standings-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
